2fe8148ea7d1379e410960f780276d1454dacc8f
bigfoodK <satcom1600@hanmail.net>
2020-05-16 06:48:50 +0000

    Place db to under server directory

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,8 +1,6 @@
 import Db from 'level-ts';
 import Path from 'path';
 
-console.log(Path.join(__dirname, 'db'));
-
-const db = new Db(Path.join(__dirname, 'db'));
+const db = new Db(Path.join(__dirname, '../db'));
 
 export default db;
